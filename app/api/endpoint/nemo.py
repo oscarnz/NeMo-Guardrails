ac6d1f2d3ad4fe33b2d69b927b61d57b7efe3480
Ariff Nazhan <58643675+oscarnz@users.noreply.github.com>
2023-07-31 22:52:43 +0800

    stateless config

--- app/api/endpoint/nemo.py
+++ app/api/endpoint/nemo.py
@@ -6,6 +6,28 @@ router = APIRouter(
     tags=["Nemo"],
 )
 
+COLANG_CONFIG = """
+define user express greeting
+  "hi"
+
+define user express insult
+  "You are stupid"
+
+# Basic guardrail against insults.
+define flow
+  user express insult
+  bot express calmly willingness to help
+
+"""
+
+YAML_CONFIG = """
+models:
+  - type: main
+    engine: openai
+    model: text-davinci-003
+"""
+
+
 # Give the path to the folder containing the rails
 config = RailsConfig.from_path("./nemo/config")
 rails = LLMRails(
@@ -27,6 +49,27 @@ async def chat(
     memory.append(history)
     
     response = await rails.generate_async(messages=memory)
+
+    memory.append(response)
+
+    return response
+
+
+@router.post("/chat-stateless")
+async def chat_without_file(
+    input: str
+):
+    config_stateless = RailsConfig.from_content(COLANG_CONFIG, YAML_CONFIG)
+    app_stateless = LLMRails(config_stateless)
+    
+    # Define role and question to be asked
+    history = {
+        "role": "user",
+        "content": input
+    }
+    memory.append(history)
+    
+    response = await app_stateless.generate_async(messages=memory)
 
     memory.append(response)
 
